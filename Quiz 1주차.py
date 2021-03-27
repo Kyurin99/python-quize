a0d05adb4405a76d520785d9150a0549f2872a97
Kyurin99 <81082637+Kyurin99@users.noreply.github.com>
2021-03-28 03:20:21 +0900

    Add files via upload

---
+++ Quiz 1주차.py
@@ -0,0 +1,16 @@
+#Quize 1
+
+
+station = "사당"
+station = "신도림"
+station = "인천공항"
+
+print(station,"행 열차가 들어오고 있습니다.")
+
+#Quize 2
+
+from random import *
+
+day = randint(4,28) # 4 ~ 28 이하의 임의의 값 생성
+print("오프라인 스터디 모임 날짜는 매월",str(day),"일로 선정되었습니다.")
+
