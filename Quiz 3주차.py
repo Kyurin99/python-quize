6f2a46ad1b948e4936aa5b83126372b971757f06
Kyurin99 <81082637+Kyurin99@users.noreply.github.com>
2021-05-12 04:15:14 +0900

    Add files via upload

--- Quiz 3주차.py
+++ Quiz 3주차.py
@@ -1,4 +1,4 @@
-#Quiz 03
+#Quiz 04
 
 from random import *
 
@@ -12,3 +12,18 @@ print("-- 당첨자 발표 --\n")
 print("치킨 당첨자 : " , win[0] )
 print("커피 당첨자 : " , win[1:])
 print("\n-- 축하합니다 --")
+
+ #Quiz 05
+
+from random import *
+
+count = 0 
+
+for i in range(1,51) :
+    time = randrange(5,51)
+    if time >= 5 and time <= 15:
+        print("[o] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+        count += 1
+    else :
+        print("[] {0}번째 손님 (소요시간 : {1}분)".format(i, time)) 
+print("총 탑승 승객 : {}".format(count))
